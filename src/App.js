103999814c4c46c0eee8584d0642e3d468b7ee6b
Mike Torii <miketorii#gmail.com>
2024-02-03 11:47:16 +0900

    modified app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Mike 1234
+          Hello Mike 999
         </p>
         <a
           className="App-link"
