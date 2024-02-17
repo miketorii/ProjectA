5cb24d62d395bbb692e55515e58c61f59959011f
Mike Torii <miketorii#gmail.com>
2024-02-17 18:42:17 +0900

    add azure function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setResponse] = useState([]);
+
+  useEffect( () => {
+    axios.get('/api/GetDevices')
+    .then( res => {
+      setResponse(res.data);
+      console.log(res);
+      } )
+    .catch( error => {
+      const mydata = 'get error ' + error;
+      setResponse(mydata); 
+      console.log(error);
+    } )
+  
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello Mike 999
+        </p>
+        <p>
+          {data}
         </p>
         <a
           className="App-link"
