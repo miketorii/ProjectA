3e533dfaa0ce01aedc1acc50630ffa02b27b84e4
Mike Torii <miketorii#gmail.com>
2024-02-17 23:03:51 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import { useEffect, useState} from 'react';
-import axios from 'axios';
 
 import Header from './components/header';
 import Footer from './components/footer';
