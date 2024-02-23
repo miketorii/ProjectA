700f76538e5f4f319e0824cbaecebb4a0a19ced4
Mike Torii <miketorii@gmail.com>
2024-02-23 14:52:37 +0900

    modified lint err

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,7 @@ class Header extends React.Component {
     return (
       <div>
 	  <nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left App-w3-header" id="mySidebar">
-	      <a href="javascript:void(0)" onClick={this.w3_close.bind(this)} class="w3-bar-item w3-button">Close Menu</a>
+	      <a href={null} onClick={this.w3_close.bind(this)} class="w3-bar-item w3-button">Close Menu</a>
 	      <Link to="/" onClick={this.w3_close.bind(this)} class="w3-bar-item w3-button">Home</Link>
 	      <Link to="/marsphotos" onClick={this.w3_close.bind(this)} class="w3-bar-item w3-button">Photos</Link>
 	      <Link to="/marsnews" onClick={this.w3_close.bind(this)} class="w3-bar-item w3-button">News</Link>
@@ -34,7 +34,7 @@ class Header extends React.Component {
 
 	  <div class="w3-bar w3-black w3-wide w3-padding w3-card">
 	      <div class="w3-button w3-padding-16 w3-left" onClick={this.w3_open.bind(this)}>☰</div>
-	      <a href="#home" class="w3-bar-item w3-button"><b>Mars Project</b></a>
+	      <Link to="/" class="w3-bar-item w3-button"><b>Mars Project</b></Link>
 	      <div class="w3-right w3-hide-small">
 		  <Link to="/" class="wd-bar-item w3-button">Home</Link>
 		  <Link to="/marsphotos" class="wd-bar-item w3-button">Photos</Link>
